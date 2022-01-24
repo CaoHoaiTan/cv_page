55e70ecdf576f9a3764508830632a9a8539cc837
CaoHoaiTan <caohoaitan@gmail.com>
2022-01-24 17:00:31 +0700

    Update file Projects

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,18 +1,41 @@
 
+const projects = [
+    {
+      id: 1,
+      name: 'Website sell sneaker(Java Servlet)',
+      linkGitHub: 'https://github.com/thanhnam2811/FashionWeb'
+    },
+    {
+      id: 2,
+      name: 'Manage sneaker stores Winform(Java)',
+      linkGitHub: 'https://github.com/CaoHoaiTan/QuanLiCuaHangBanGiay'
+    },
+    {
+      id: 3,
+      name: 'App English card (Flutter dart)',
+      linkGitHub: 'https://github.com/CaoHoaiTan/app_english_card'
+    },
+    {
+        id: 4,
+        name: 'Website sell fashion clothes',
+        linkGitHub: 'https://github.com/thanhnam2811/SneakersWeb'
+      },
+  
+  ]
+  
 function Projects(){
 
     return(
       <div  className="project">
            <ul>
-              <li className = "web">
-              <a href="" className="github"> <i className="fab fa-github-alt"> </i>Web bán giày sneaker</a>
-              </li>
-              <li className = "web">
-              <a href="" className="github"> <i className="fab fa-github-alt"> </i>Web bán giày sneaker</a>
-              </li>
-              <li className = "web">
-              <a href="" className="github"> <i className="fab fa-github-alt"> </i>Web bán giày sneaker</a>
-              </li>
+                {
+                  projects.map((project) => (
+                    <li className = {project.id}>
+                    <a href={project.linkGitHub} className= {project.id}> <i className="fab fa-github-alt"> </i>{project.name}</a>
+                    </li>
+                  ))
+                 }
+
            </ul> 
                                       
       </div>
